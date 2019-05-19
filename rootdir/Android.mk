89ae00104634b2fb87866f0e3c73616402da71bf
Andrey S. Shidakov <ashtmb@gmail.com>
2019-05-19 09:35:04 +0000

    Added Built-in Spectrum Support

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -93,6 +93,14 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.spectrum.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.spectrum.sh
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT) 
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.sanders.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
